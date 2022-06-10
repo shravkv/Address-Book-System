70b934f058c5b1060b73c21d6de0152e125d4079
sravan <sravan.k.vodnala@gmail.com>
2022-06-10 17:48:57 +0530

    Added: creating contact(getter and setter)

--- src/Book.java
+++ src/Book.java
@@ -9,9 +9,6 @@ public class Book {
     private int phoneNum;
     private int zip;
 
-
-
-
     public Book(String firstName, String lastName, String address, String email, int phoneNum, int zip) {
         this.firstName = firstName;
         this.lastName = lastName;
